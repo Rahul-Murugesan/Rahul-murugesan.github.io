ac2449c0a59c6a0a79a6b55073b9d45eec302cc9
RahulM-3 <rahulcode97@gmail.com>
2023-08-26 13:40:43 +0530

    template update

--- others/blogs/blog.js
+++ others/blogs/blog.js
@@ -1,29 +1,64 @@
 const mangaurl = "https://api.jikan.moe/v4/manga/";
+const animeurl = "https://api.jikan.moe/v4/anime/"
 
 window.addEventListener("load", async function(){
-	const url = mangaurl+document.getElementById("eid").innerText;
-	const response = await fetch(url);
-	var data = await response.json();
-	data = data.data
+	if(document.getElementById("etype").innerText == "manga"){
+		const url = mangaurl+document.getElementById("eid").innerText;
+		const response = await fetch(url);
+		var data = await response.json();
+		data = data.data;
 
-	document.getElementById("name").innerHTML = data.title;
+		var div = document.getElementById("einfo");
+		document.getElementById("name").innerHTML = data.title;
+		if(document.getElementById("img").src == window.location.href){
+			document.getElementById("img").src = data.images.jpg.image_url.replace("/", "");
+		}
 
-	console.log(data.authors);
-	data.authors.forEach(function(item){
-		document.getElementById("auth").innerHTML += `<br>&nbsp&nbsp${item.name}`
-	})
+		data.authors.forEach(function(item){
+			div.innerHTML += `<h6><span>Authors & Studios:<br>&nbsp&nbsp${item.name}</span></h6>`
+		})
+		div.innerHTML += `<h6><span>Type:&nbsp${data.type}</span></h6>`;
+		div.innerHTML += `<h6><span>Published:&nbsp${data.published.string}</span></h6>`;
+		div.innerHTML += `<h6><span>Status:&nbsp${data.status}</span></h6>`;
+		div.innerHTML += `<h6><span>Chapters:&nbsp${data.chapters}</span></h6>`;
+		div.innerHTML += `<h6><span>Volumes:&nbsp${data.volumes}</span></h6>`;
+		if(data.demographics.length){
+			div.innerHTML += `<h6><span>Demographics:&nbsp${data.demographics[0].name}</span></h6>`;
+		}
+		div.innerHTML += '<section class="heading"><h4>MAL Statistics</h4><div class="bar barheading"></div></section>';
+		div.innerHTML += `<h6><span>Score:&nbsp${data.score} (scored by ${data.scored_by} users)</span></h6>`;
+		div.innerHTML += `<h6 id="rank"><span>Ranked:&nbsp${data.rank}</span></h6>`;
+		div.innerHTML += `<h6 id="pop"><span>Popularity:&nbsp${data.popularity}</span></h6>`;
+		div.innerHTML += `<h6 id="mem"><span>Members:&nbsp${data.members}</span></h6>`;
+	}
+	else if(document.getElementById("etype").innerText == "anime"){
+		url = animeurl+document.getElementById("eid").innerText;
+		const response = await fetch(url);
+		var data = await response.json();
+		data = data.data;
 
-	document.getElementById("type").innerHTML += data.type;
-	document.getElementById("pubfrm").innerHTML += data.published.string;
-	document.getElementById("sts").innerHTML += data.status;
-	document.getElementById("chap").innerHTML += data.chapters;
-	document.getElementById("vol").innerHTML += data.volumes;
-	document.getElementById("demog").innerHTML += data.demographics[0].name;
+		var div = document.getElementById("einfo");
+		document.getElementById("name").innerHTML = data.title;
+		if(document.getElementById("img").src == window.location.href){
+			document.getElementById("img").src = data.images.jpg.image_url.replace("/", "");
+		}
 
-	document.getElementById("score").innerHTML += `${data.score} (scored by ${data.scored_by} users)`;
-	document.getElementById("rank").innerHTML += data.rank;
-	document.getElementById("pop").innerHTML += data.popularity;
-	document.getElementById("mem").innerHTML += data.members;
+		data.studios.forEach(function(item){
+			div.innerHTML += `<h6><span>Studios:<br>&nbsp&nbsp${item.name}</span></h6>`
+		})
+		div.innerHTML += `<h6><span>Type:&nbsp${data.type}</span></h6>`;
+		div.innerHTML += `<h6><span>Aired:&nbsp${data.aired.string}</span></h6>`;
+		div.innerHTML += `<h6><span>Status:&nbsp${data.status}</span></h6>`;
+		div.innerHTML += `<h6><span>Episodes:&nbsp${data.episoded}</span></h6>`;
+		if(data.demographics.length){
+			div.innerHTML += `<h6><span>Demographics:&nbsp${data.demographics[0].name}</span></h6>`;
+		}
+		div.innerHTML += '<section class="heading"><h4>MAL Statistics</h4><div class="bar barheading"></div></section>';
+		div.innerHTML += `<h6><span>Score:&nbsp${data.score} (scored by ${data.scored_by} users)</span></h6>`;
+		div.innerHTML += `<h6 id="rank"><span>Ranked:&nbsp${data.rank}</span></h6>`;
+		div.innerHTML += `<h6 id="pop"><span>Popularity:&nbsp${data.popularity}</span></h6>`;
+		div.innerHTML += `<h6 id="mem"><span>Members:&nbsp${data.members}</span></h6>`;
+	}
 
 	document.getElementById("loaded").innerText = "true";
 
