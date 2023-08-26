47093c03a7a42d63cdbcfc2d49fa5a4e1fcc45de
RahulM-3 <rahulcode97@gmail.com>
2023-08-27 01:05:25 +0530

    template update

--- others/blogs/blog.js
+++ others/blogs/blog.js
@@ -14,22 +14,24 @@ window.addEventListener("load", async function(){
 			document.getElementById("img").src = data.images.jpg.image_url.replace("\\", "");
 		}
 
+		var auths = "";
 		data.authors.forEach(function(item){
-			div.innerHTML += `<h6><span>Authors & Studios:<br>&nbsp&nbsp${item.name}</span></h6>`
+			auths += `<br>&nbsp&nbsp${item.name}`;
 		})
-		div.innerHTML += `<h6><span>Type:&nbsp${data.type}</span></h6>`;
-		div.innerHTML += `<h6><span>Published:&nbsp${data.published.string}</span></h6>`;
-		div.innerHTML += `<h6><span>Status:&nbsp${data.status}</span></h6>`;
-		div.innerHTML += `<h6><span>Chapters:&nbsp${data.chapters}</span></h6>`;
-		div.innerHTML += `<h6><span>Volumes:&nbsp${data.volumes}</span></h6>`;
+		div.innerHTML += `<h6><span>Authors & Studios:</span>${auths}</h6>`;
+		div.innerHTML += `<h6><span>Type:&nbsp</span>${data.type}</h6>`;
+		div.innerHTML += `<h6><span>Published:&nbsp</span>${data.published.string}</h6>`;
+		div.innerHTML += `<h6><span>Status:&nbsp</span>${data.status}</h6>`;
+		div.innerHTML += `<h6><span>Chapters:&nbsp</span>${data.chapters}</h6>`;
+		div.innerHTML += `<h6><span>Volumes:&nbsp</span>${data.volumes}</h6>`;
 		if(data.demographics.length){
-			div.innerHTML += `<h6><span>Demographics:&nbsp${data.demographics[0].name}</span></h6>`;
+			div.innerHTML += `<h6><span>Demographics:&nbsp</span>${data.demographics[0].name}</h6>`;
 		}
 		div.innerHTML += '<section class="heading"><h4>MAL Statistics</h4><div class="bar barheading"></div></section>';
-		div.innerHTML += `<h6><span>Score:&nbsp${data.score} (scored by ${data.scored_by} users)</span></h6>`;
-		div.innerHTML += `<h6 id="rank"><span>Ranked:&nbsp${data.rank}</span></h6>`;
-		div.innerHTML += `<h6 id="pop"><span>Popularity:&nbsp${data.popularity}</span></h6>`;
-		div.innerHTML += `<h6 id="mem"><span>Members:&nbsp${data.members}</span></h6>`;
+		div.innerHTML += `<h6><span>Score:&nbsp</span>${data.score} <font size="1">(scored by ${data.scored_by} users)</font></h6>`;
+		div.innerHTML += `<h6 id="rank"><span>Ranked:&nbsp</span>${data.rank}</h6>`;
+		div.innerHTML += `<h6 id="pop"><span>Popularity:&nbsp</span>${data.popularity}</h6>`;
+		div.innerHTML += `<h6 id="mem"><span>Members:&nbsp</span>${data.members}</h6>`;
 	}
 	else if(document.getElementById("etype").innerText == "anime"){
 		url = animeurl+document.getElementById("eid").innerText;
@@ -43,23 +45,27 @@ window.addEventListener("load", async function(){
 			document.getElementById("img").src = data.images.jpg.image_url.replace("\\", "");
 		}
 
+		var stds = "";
 		data.studios.forEach(function(item){
-			div.innerHTML += `<h6><span>Studios:<br>&nbsp&nbsp${item.name}</span></h6>`
+			stds += `<br>&nbsp&nbsp${item.name}`;
 		})
-		div.innerHTML += `<h6><span>Type:&nbsp${data.type}</span></h6>`;
-		div.innerHTML += `<h6><span>Aired:&nbsp${data.aired.string}</span></h6>`;
-		div.innerHTML += `<h6><span>Status:&nbsp${data.status}</span></h6>`;
-		div.innerHTML += `<h6><span>Episodes:&nbsp${data.episodes}</span></h6>`;
+		div.innerHTML += `<h6><span>Authors:</span>${stds}</h6>`;
+		div.innerHTML += `<h6><span>Type:&nbsp</span>${data.type}</h6>`;
+		div.innerHTML += `<h6><span>Aired:&nbsp</span>${data.aired.string}</h6>`;
+		div.innerHTML += `<h6><span>Status:&nbsp</span>${data.status}</h6>`;
+		div.innerHTML += `<h6><span>Episodes:&nbsp</span>${data.episodes}</h6>`;
 		if(data.demographics.length){
-			div.innerHTML += `<h6><span>Demographics:&nbsp${data.demographics[0].name}</span></h6>`;
+			div.innerHTML += `<h6><span>Demographics:&nbsp</span>${data.demographics[0].name}</h6>`;
 		}
 		div.innerHTML += '<section class="heading"><h4>MAL Statistics</h4><div class="bar barheading"></div></section>';
-		div.innerHTML += `<h6><span>Score:&nbsp${data.score} (scored by ${data.scored_by} users)</span></h6>`;
-		div.innerHTML += `<h6 id="rank"><span>Ranked:&nbsp${data.rank}</span></h6>`;
-		div.innerHTML += `<h6 id="pop"><span>Popularity:&nbsp${data.popularity}</span></h6>`;
-		div.innerHTML += `<h6 id="mem"><span>Members:&nbsp${data.members}</span></h6>`;
+		div.innerHTML += `<h6><span>Score:&nbsp</span>${data.score} <font size="1">(scored by ${data.scored_by} users)</font></h6>`;
+		div.innerHTML += `<h6 id="rank"><span>Ranked:&nbsp</span>${data.rank}</h6>`;
+		div.innerHTML += `<h6 id="pop"><span>Popularity:&nbsp</span>${data.popularity}</h6>`;
+		div.innerHTML += `<h6 id="mem"><span>Members:&nbsp</span>${data.members}</h6>`;
 	}
 
 	document.getElementById("loaded").innerText = "true";
+
+	document.getElementById("mainframe").innerHTML += "<h6><font color='red'>[NOTE]</font> Not all these words are mine. I'm still trying to improve my writting skills with the help of AI.</h6>";
 
 })
