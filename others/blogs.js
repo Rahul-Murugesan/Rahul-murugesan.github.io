a49521ae6fbf0838fb5864295636dd9b554b1c6e
RahulM-3 <rahulcode97@gmail.com>
2023-08-28 06:57:06 +0530

    working filter update

--- others/blogs.js
+++ others/blogs.js
@@ -39,31 +39,52 @@ function ExpandBtn(ele){
     }
 }
 
-var option = "Recent"
-function filter(ele){
-    filteropt = ele.innerText;
-    option = ele.innerText
-    console.log(option)
-    ExpandBtn(document.getElementById("filterbtn"))
-}
-
-var load = 1;
-var blog = []
+var load;
+var blog = [];
 var totale = 0;
-window.addEventListener("load", async function(){
+var option = "Recent"
+async function loaddata(){
+    load = 1;
+    blog = []
+    totale = 0;
+
     const url = "./blogs.json"
     const response = await fetch(url);
     var data = await response.json();
-    data = data.all;
-    totale = data.length
-    for(var i=0; i<4; i++){
-        blog.push(`blogs/${data[i]}.html`);
+    if(option == "Recent"){
+        data = data.all;
     }
-    document.getElementById("blogcnt").data=blog[0];
-})
+    else if(option == "Aniem/Manga"){
+        data = data.animanga;
+    }
+    else if(option == "TV Series"){
+        data = data.tvseries;
+    }
+    else if(option == "Movies"){
+        data = data.movies;
+    }
+    else if(option == "Tech"){
+        data = data.tech;
+    }
+    totale = Math.min(data.length, 4)
+    if(totale == 0){
+        for(var i=3; i<document.getElementById("blogdis").childNodes.length; i+=2){
+            document.getElementById("blogdis").childNodes[i].removeAttribute("hidden");
+        }
+        document.getElementById("cntloadingscreen").setAttribute("hidden", "hidden");
+    }
+    else{
+        for(var i=0; i<totale; i++){
+            blog.push(`blogs/${data[i]}.html`);
+        }
+        document.getElementById("body").innerHTML += '<object id="blogcnt" data="" onload="fill()" style="position: absolute; top: -1000%"></object>'
+        document.getElementById("blogcnt").data=blog[0];
+    }
+}
+loaddata();
 
 async function fill(){
-    if(load <= Math.min(totale, 4)){
+    if(load <= totale){
         cnt = document.getElementById("blogcnt").contentWindow.document;
         while(cnt.getElementById("loaded").innerText == "false"){
             cnt = document.getElementById("blogcnt").contentWindow.document;
@@ -74,23 +95,42 @@ async function fill(){
         const blogtitle = cnt.getElementById("blogtitle").innerText;
         const datetime = cnt.getElementById("updatedatetime").innerText;
         const poster = cnt.getElementById("img").src;
-        document.getElementById(`poster${load}`).src = poster
-        document.getElementById(`row${load}`).innerHTML += `<div class="info"> <a href=${src}><h3 align="left">${name}: <span>${blogtitle}</span></h3></a> <h6 align="left">${datetime}</h6> </div>`;
+        document.getElementById(`row${load}`).innerHTML = `<img class="poster" id="poster${load}" onclick="expandimg(this)" src=""><div class="info"><a href=${src}><h3 align="left">${name}: <span>${blogtitle}</span></h3></a> <h6 align="left">${datetime}</h6></div>`;
+        document.getElementById(`poster${load}`).src = poster;
 
         document.getElementById("blogcnt").data=blog[load];
         load += 1
         if(load > totale){
-            document.getElementById("blogcnt").data = "";
+            console.log(document.getElementById("blogdis").childNodes)
             for(var i=3; i<document.getElementById("blogdis").childNodes.length; i+=2){
                 document.getElementById("blogdis").childNodes[i].removeAttribute("hidden");
             }
             for(var i=3; i<document.getElementById("blogdis").childNodes.length; i+=4){
                 document.getElementById("blogdis").childNodes[i].style.display = "flex";
             }
-            lscr = document.getElementById("cntloadingscreen");
-            lscr.parentNode.removeChild(lscr);
+            const obj = document.getElementById("blogcnt");
+            obj.parentNode.removeChild(obj);
+            document.getElementById("cntloadingscreen").setAttribute("hidden", "hidden");
         }
     }
+}
+
+function filter(ele){
+    filteropt = ele.innerText;
+    option = ele.innerText
+    ExpandBtn(document.getElementById("filterbtn"))
+    document.getElementById("cntloadingscreen").removeAttribute("hidden");
+    for(var i=3; i<document.getElementById("blogdis").childNodes.length; i+=2){
+        document.getElementById("blogdis").childNodes[i].setAttribute("hidden", "hidden");
+    }
+    for(var i=3; i<document.getElementById("blogdis").childNodes.length; i+=4){
+        document.getElementById("blogdis").childNodes[i].style.display = "none";
+    }
+    for(var i=1; i<=4; i++){
+        document.getElementById(`row${i}`).innerHTML = "";
+    }
+    loaddata();
+    fill();
 }
 
 function expandimg(ele){
