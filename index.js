b51059ae3ad37d2958e39fd1577e40dee87bd071
RahulM-3 <rahulcode97@gmail.com>
2024-09-30 17:50:33 +0530

    updated log format

--- index.js
+++ index.js
@@ -51,6 +51,47 @@ function togglemode(ele){
 }
 
 
+// Detect browser name
+function getBrowser() {
+    const userAgent = navigator.userAgent;
+    let browserName;
+
+    if (userAgent.indexOf("Chrome") > -1) {
+        browserName = "Chrome";
+    } else if (userAgent.indexOf("Firefox") > -1) {
+        browserName = "Firefox";
+    } else if (userAgent.indexOf("Safari") > -1) {
+        browserName = "Safari";
+    } else if (userAgent.indexOf("MSIE") > -1 || userAgent.indexOf("Trident") > -1) {
+        browserName = "Internet Explorer";
+    } else {
+        browserName = "Unknown";
+    }
+
+    return browserName;
+}
+// Detect OS name
+function getOS(){
+    const userAgent = navigator.userAgent;
+    let osName;
+
+    if (userAgent.indexOf("Win") > -1) {
+        osName = "Windows";
+    } else if (userAgent.indexOf("Mac") > -1) {
+        osName = "MacOS";
+    } else if (userAgent.indexOf("X11") > -1 || userAgent.indexOf("Linux") > -1) {
+        osName = "Linux";
+    } else if (userAgent.indexOf("Android") > -1) {
+        osName = "Android";
+    } else if (userAgent.indexOf("like Mac") > -1) {
+        osName = "iOS";
+    } else {
+        osName = "Unknown";
+    }
+
+    return osName;
+}
+
 // write access log
 async function write_access_log(){
     // get public ip
@@ -67,7 +108,25 @@ async function write_access_log(){
     const url = window.location.href;
     const time = new Date().toString().replace(' GMT+0530 (India Standard Time)','');
 
-    const log = {"Access log":[time, url, publicip]};
+    const userAgent = navigator.userAgent;
+    let browserName, osName;
+
+    // Detect OS name
+    if (userAgent.indexOf("Win") > -1) {
+        osName = "Windows";
+    } else if (userAgent.indexOf("Mac") > -1) {
+        osName = "MacOS";
+    } else if (userAgent.indexOf("X11") > -1 || userAgent.indexOf("Linux") > -1) {
+        osName = "Linux";
+    } else if (userAgent.indexOf("Android") > -1) {
+        osName = "Android";
+    } else if (userAgent.indexOf("like Mac") > -1) {
+        osName = "iOS";
+    } else {
+        osName = "Unknown";
+    }
+
+    const log = ["Access log", publicip, time, url, getBrowser(), getOS()];
     
     const linux_server_api = "https://linux-server-api-default-rtdb.firebaseio.com/log.json";
     // Make a POST request using fetch
