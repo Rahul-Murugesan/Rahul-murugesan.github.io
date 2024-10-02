22b4bdb15b087dbde2abc02f1aae87864a95a8d8
RahulM-3 <rahulcode97@gmail.com>
2024-10-02 18:51:44 +0530

    encrypt credentials before sending to api

--- services.js
+++ services.js
@@ -14,11 +14,33 @@ function changeform() {
     }
 }
 
+async function encrypt(cred) {
+    const response = await fetch('public_key.pem');
+    if (!response.ok) {
+        throw new Error('Failed to load public key');
+    }
+    const publicKey = forge.pki.publicKeyFromPem(await response.text());
+    return forge.util.encode64(publicKey.encrypt(cred, 'RSA-OAEP'));
+}
+
+function generateSessionId() {
+    const timestamp = Date.now(); // Get the current timestamp
+    const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = Math.random() * 16 | 0; // Generate a random number between 0 and 15
+        const v = c === 'x' ? r : (r & 0x3 | 0x8); // Create the UUID structure
+        return v.toString(16); // Convert to hexadecimal
+    });
+    return `${timestamp}-${uuid}`; // Combine timestamp with UUID
+}
+
 var linux_server_api = "https://linux-server-api-default-rtdb.firebaseio.com/";
-function login() {
-    const email = document.getElementById("loginemail").value;
-    const password = document.getElementById("loginpassword").value;
-    const request = {"login":[email, password]}
+async function login() {
+    const username = await encrypt(document.getElementById("loginusername").value);
+    const password = await encrypt(document.getElementById("loginpassword").value);
+    n = generateSessionId();
+    console.log(n)
+    const sessionid = await encrypt(n);
+    const request = {"login":[username, password, sessionid]}
 
     fetch(linux_server_api+"user_request.json", {
         method: 'POST',
@@ -26,13 +48,16 @@ function login() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(request)
+    }).then(response => response.json()) // Parse the response as JSON
+    .then(data => {
+        reqid = data.name;
     })
 }
 
-function register() {
-    const email = document.getElementById("registeremail").value;
-    const password = document.getElementById("registerpassword").value;
-    const request = {"register":[email, password]}
+async function register() {
+    const username = await encrypt(document.getElementById("registerusername").value);
+    const password = await encrypt(document.getElementById("registerpassword").value);
+    const request = {"register":[username, password]}
 
     fetch(linux_server_api+"user_request.json", {
         method: 'POST',
