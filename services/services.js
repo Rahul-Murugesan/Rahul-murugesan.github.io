673ab20bc8316d69c47a21b00131898fdc6cd46c
RahulM-3 <rahulcode97@gmail.com>
2024-10-08 08:45:38 +0530

    display server status

--- services/services.js
+++ services/services.js
@@ -14,3 +14,45 @@ function changeform() {
         loginForm.hidden = true;
     }
 }
+
+// linux server api link
+var linux_server_api = "https://linux-server-api-default-rtdb.firebaseio.com/";
+
+// check server status
+setInterval( async () => {
+    const response = await fetch(linux_server_api+'server_status.json');
+    const server_status = JSON.parse(await response.text()).last_active * 1000;
+
+    const timeDifference = (Date.now() - server_status);
+    if (timeDifference >= 10000) {
+        
+        let seconds = Math.floor(timeDifference / 1000);
+        const hours = Math.floor(seconds / 3600);
+        seconds %= 3600;
+        const minutes = Math.floor(seconds / 60);
+        seconds %= 60;
+
+        let result = '';
+
+        if (hours > 0) {
+            result += `${hours} hr${hours > 1 ? 's' : ''} `;
+        }
+        if (minutes > 0) {
+            result += `${minutes} min${minutes > 1 ? 's' : ''} `;
+        }
+        if (seconds > 0 || result === '') {
+            result += `${seconds} sec${seconds > 1 ? 's' : ''} `;
+        }
+
+        document.getElementById("cursts").innerText = "🔴";
+        document.getElementById("lastactive").innerText = `Last Active: ${result.trim()} ago`;
+        document.getElementById("loginbutton").disabled = true;
+        document.getElementById("registerbutton").disabled = true;
+    } else {
+        document.getElementById("cursts").innerText = "🟢";
+        document.getElementById("lastactive").innerText = "";
+        document.getElementById("loginbutton").disabled = false;
+        document.getElementById("registerbutton").disabled = false;
+    }
+
+}, 1000);
